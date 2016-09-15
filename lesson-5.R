7ca0e0b3a692c8e52b1358ea3f447ab0369d2f1e
Appler <Jessica.Appler@hhs.gov>
2016-09-15 12:31:21 -0400

    Lunch break in Lesson 5

--- lesson-5.R
+++ lesson-5.R
@@ -68,38 +68,52 @@ ggplot(data = (surveys %>% filter(species_id == "DM")) ,
 
 ## Axes, labels and themes
 
-histo <- ggplot(data = surveys_dm,
+histo <- ggplot(data = (surveys %>% filter(species_id == "DM")) ,
                 aes(x = weight, fill = sex)) +
-  geom_...
+  geom_histogram(
+    stat = "bin",
+    binwidth = 3,
+    color = "white")+
+  scale_fill_manual(values = c("magenta", "darkblue"))
 histo
 
+##Adding in the labels
+
 histo <- histo +
-  ...(title = "Dipodomys merriami weight distribution",
+  labs(title = "Dipodomys merriami weight distribution",
        x = "Weight (g)",
        y = "Count") +
-  scale_x_continuous(limits = c(20, 60),
-                     breaks = c(20, 30, 40, 50, 60))
+  scale_x_continuous(breaks = c(20, 30, 40, 50, 60))
 histo
 
-histo <- histo +
+##The solution for picky plotters!
+histobw <- histo +
   theme_bw() +
   theme(legend.position = c(0.2, 0.5),
-        plot.title = ...,
-        ... = element_text(...),
-        ... = element_text(size = 13, vjust = 0))
-histo
+        plot.title = element_text(face = "bold", vjust = 2),
+        axis.title.y = element_text(size = 13, vjust = 1),
+        axis.title.x = element_text(size = 13, vjust = 0))
+histobw
+
+themetoBW <- theme_bw() +
+  theme(legend.position = c(0.2, 0.5),
+        plot.title = element_text(face = "bold", vjust = 2),
+        axis.title.y = element_text(size = 13, vjust = 1),
+        axis.title.x = element_text(size = 13, vjust = 0))
+histoBW2 <- histo + themetoBW
+histoBW2
 
 ## Facets
 
-surveys_dm$month <- as.factor(surveys_dm$month)
-levels(surveys_dm$month) <- c("January", "February", "March", "April", "May", "June",
+surveys$month <- as.factor(surveys$month)
+levels(surveys$month) <- c("January", "February", "March", "April", "May", "June",
                               "July", "August", "September", "October", "November", "December")
 
-ggplot(data = surveys_dm,
-       aes(x = weight)) +
-  geom_histogram() +
-  ...
-  labs(title = "DM weight distribution by month",
+ggplot((data = (surveys %>% filter(species_id == "DM"))),
+        aes(x = weight)) +
+         geom_histogram(binwidth = 4, color ="white", fill = "darkblue") +
+         facet_wrap(~month) +
+   labs(title = "DM weight distribution by month",
        x = "Count",
        y = "Weight (g)")
 
